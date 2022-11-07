30fefb6fbc0fd4b8b7381c573e1ab5e6108027de
3a932111 <3a932111@gm.student.ncut.edu.tw>
2022-11-07 09:08:07 +0800

    4 設定 route 及對應的 Controller+5 設定 Controller 回傳對應的 view

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PostController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect(route('posts.index'));
 });
+
+Route::get ('posts',[PostController::class,'index'])->name('posts.index');
+Route::get ('post',[PostController::class,'show'])->name('posts.show');
+Route::get ('contact',[PostController::class,'contact'])->name('posts.contact');
+Route::get ('about',[PostController::class,'about'])->name('posts.about');
