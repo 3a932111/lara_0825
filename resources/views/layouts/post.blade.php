7723176a7d2c663a79bb47f2f675beb6f3c23de6
3a932111 <3a932111@gm.student.ncut.edu.tw>
2022-11-07 08:27:55 +0800

    3-3 修改子樣板 contact.blade.php (contact 視圖) 套用主樣板

--- resources/views/layouts/post.blade.php
+++ resources/views/layouts/post.blade.php
@@ -1,23 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>Clean Blog - Sample  Post</title>
-        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
-        <!-- Font Awesome icons (free version)-->
-        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
-        <!-- Google fonts-->
-        <link href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css" />
-        <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="css/styles.css" rel="stylesheet" />
-    </head>
-    <body>
-    @extends('layouts.master')
-    @section('title', 'Clean Blog - Home')
+@extends('layouts.master')
+    @section('title', 'Clean Blog - Sample Post')
     @section('content')
         <!-- Page Header-->
         <header class="masthead" style="background-image: url('{{asset('assets/img/post-bg.jpg')}}')">
@@ -69,5 +51,3 @@
             </div>
         </article>
     @endsection
-    </body>
-</html>
