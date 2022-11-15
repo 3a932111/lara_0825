9828473603100f0012032aac317b30d799eaec85
3a932111 <3a932111@gm.student.ncut.edu.tw>
2022-11-15 19:06:20 +0800

    0825 練習3 查詢資料

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
-use App\Models\Post;
+use App\Models\post;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,21 +15,33 @@ use App\Models\Post;
 |
 */
 
-/*用post方式新增資料
- * Route::get('/', function () {
-    //return redirect(route('posts.index'));
-    $post=new Post();
-    $post->title='test title';
-    $post->content='test content';
-    $post->save();
-    return 'save,OK';
-});*/
 
-/*用create方式新增資料*/
+  Route::get('/', function () {
+     /*用post方式新增資料
+        //return redirect(route('posts.index'));
+        $post=new Post();
+        $post->title='test title';
+        $post->content='test content';
+        $post->save();
+        return 'save,OK';
+    */
+
+/*用create方式新增資料
 post::create([
     'title'=>'created title',
     'content'=>'created content',
 ]);
+*/
+      /*$posts=Post::all();
+      dd($posts);*/
+
+      /*$post=Post::find(1);
+        dd($post);*/
+
+    /*$posts=Post::where('id','<',10)->orderBy('id','DESC')->get();
+      dd($posts);*/
+});
+
 
 Route::get ('posts',[PostController::class,'index'])->name('posts.index');
 Route::get ('post',[PostController::class,'show'])->name('posts.show');
